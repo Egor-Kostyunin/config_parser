637509a5da3220d8e27720ed87db9f66c8bd56fd
Kostyunin Egor <kostyunin.eg@yandex.ru>
2024-01-26 11:01:16 +0300

    +

--- src/get_config_values.c
+++ src/get_config_values.c
@@ -1,5 +1,6 @@
 #include "config_parser.h"
 #include <string.h>
+#include <stdio.h>
 
 values_list* GetConfigValues(config *config_ptr, char *section_name, char *variable_name) {
 	for(config *current = config_ptr; current != NULL; current = current->next) {
